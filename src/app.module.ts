77c41e0dbb18744c6c7c7ba356231865d1346b48
0xMahabub <mahabub212002072@gmail.com>
2022-12-18 17:21:50 +0600

    refactor:rebase

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
 import { ApiModule } from './api/api.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [PrismaModule, ApiModule],
+  imports: [ApiModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
